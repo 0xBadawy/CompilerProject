dd30fd6a5b4145a12da745e5d1f13eab4fe65287
0xBadawy <mohamedbadawysayed02@gmail.com>
2024-12-19 19:55:50 +0200

    Update GUI layout and styling for Lex/Yacc calculator

--- Gui/py.py
+++ Gui/py.py
@@ -10,26 +10,27 @@ def run_calculator():
     result_output.insert(tk.END, stdout.decode())  
 
 ctk.set_appearance_mode("light")  
-ctk.set_default_color_theme("green") 
+ctk.set_default_color_theme("blue") 
 
 root = ctk.CTk()  
-root.title("Lex/Yacc Calculator")
-root.geometry("800x800")  
+root.title("Compiler Project")
+root.geometry("1400x800")  
 
-root.grid_rowconfigure(0, weight=1, minsize=50)  
-root.grid_rowconfigure(1, weight=0, minsize=50) 
-root.grid_rowconfigure(2, weight=1, minsize=50)  
-root.grid_columnconfigure(0, weight=1, minsize=200) 
-root.grid_columnconfigure(1, weight=0, minsize=200)  
+root.grid_rowconfigure(0, weight=1, minsize=5)  
+root.grid_rowconfigure(1, weight=0, minsize=5) 
+root.grid_columnconfigure(0, weight=1, minsize=5) 
+root.grid_columnconfigure(1, weight=0, minsize=5)  
+root.grid_columnconfigure(2, weight=1, minsize=5)
 
-text_input = ctk.CTkTextbox(root, height=10, width=30, font=("Helvetica", 12), wrap=tk.WORD, corner_radius=8)
+text_input = ctk.CTkTextbox(root, height=10, width=30, font=("Consolas", 19, "bold"), wrap=tk.WORD, corner_radius=8)
 text_input.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
 
-run_button = ctk.CTkButton(root, text="Run Calculation", command=run_calculator, width=200, height=40, font=("Helvetica", 12, "bold"))
-run_button.grid(row=1, column=0, padx=20, pady=10, sticky="nsew")
+result_output = ctk.CTkTextbox(root, height=10, width=30, font=("Consolas", 19, "bold"), wrap=tk.WORD, corner_radius=8)
+result_output.grid(row=0, column=1, columnspan=3, padx=10, pady=10, sticky="nsew")
 
-result_output = ctk.CTkTextbox(root, height=10, width=30, font=("Helvetica", 12), wrap=tk.WORD, corner_radius=8)
-result_output.grid(row=2, column=0, padx=20, pady=10, sticky="nsew")
+run_button = ctk.CTkButton(root, text="Run Code", command=run_calculator, width=200, height=40, font=("Consolas", 12, "bold"))
+run_button.grid(row=2, column=0, padx=20, pady=10)
+
 
 
 root.mainloop()
