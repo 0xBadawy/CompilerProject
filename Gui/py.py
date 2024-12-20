996bc969bcecdde4ddcdf8693593704f3aca06ee
0xBadawy <mohamedbadawysayed02@gmail.com>
2024-12-20 09:08:44 +0200

    Refactor GUI code: move to separate file and update run functionality

--- Gui/py.py
+++ Gui/py.py
@@ -5,16 +5,12 @@ import os
 
 def compile_program():
     commands = [
-        
         "flex LexFile.l",
         "yacc -d YaccFile.y",
         "gcc lex.yy.c -o lexer",
         "gcc YaccFile.tab.c lex.yy.c -o program",
     ]
-    
-    # open current directory
     print("Opening current directory" + os.getcwd())
-
     for command in commands:      
         print(f"Running: {command}")
         result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -23,12 +19,12 @@ def compile_program():
 
 def run_calculator():
     user_input = text_input.get("1.0", tk.END).strip()  
-    process = subprocess.Popen(["program.exe"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(["Compiler\\program.exe"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate(input=user_input.encode())  
     result_output.delete("1.0", tk.END)  
     result_output.insert(tk.END, stdout.decode())  
 
-ctk.set_appearance_mode("light")  
+ctk.set_appearance_mode("dark")  
 ctk.set_default_color_theme("blue") 
 
 root = ctk.CTk()  
@@ -47,8 +43,8 @@ text_input.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
 result_output = ctk.CTkTextbox(root, height=10, width=30, font=("Consolas", 19, "bold"), wrap=tk.WORD, corner_radius=8)
 result_output.grid(row=0, column=1, columnspan=3, padx=10, pady=10, sticky="nsew")
 
-compile_button = ctk.CTkButton(root, text="Compile", command=compile_program, width=200, height=40, font=("Consolas", 12, "bold"))
-compile_button.grid(row=1, column=0, padx=20, pady=10)
+#compile_button = ctk.CTkButton(root, text="Compile", command=compile_program, width=200, height=40, font=("Consolas", 12, "bold"))
+#compile_button.grid(row=1, column=0, padx=20, pady=10)
 
 run_button = ctk.CTkButton(root, text="Run Code", command=run_calculator, width=200, height=40, font=("Consolas", 12, "bold"))
 run_button.grid(row=2, column=0, padx=20, pady=10)
